976cf5a978536fb43de3abd7ca44c00b04a45d71
Roloya28 <choej9742@naver.com>
2025-01-09 15:57:54 +0900

    Change comments

--- src/main/java/com/example/calculator/App.java
+++ src/main/java/com/example/calculator/App.java
@@ -25,10 +25,9 @@ public class App {
             System.out.println("결과: " + firstNo + " " + fbo + " " + secondNo + " = " + result);
 
             // 결과값 저장, 현재 저장된 값 출력 및 삭제 여부 출력
-            System.out.println("현재 저장된 결과 수 [값]: " + calculator.getResults().size() + " " + calculator.getResults());
+            System.out.println("현재 저장된 결과 : " + calculator.getResults().size() + " " + calculator.getResults());
 
-            System.out.println("가장 먼저 저장한 값을 삭제 하시겠습니까?");
-            System.out.println("(delete 입력시 삭제 or 아무키 입력으로 continue)");
+            System.out.println("가장 먼저 저장한 값을 삭제 하시겠습니까? (delete 입력시 삭제 or 아무키 입력으로 continue)");
             String delete = sc.next();
             if (delete.equalsIgnoreCase("delete")) { // equalIgnoreCase = 대소문자 구분없이 입력값 인식
                 if (!calculator.getResults().isEmpty()) {
@@ -37,7 +36,7 @@ public class App {
                 }
             }
 
-            System.out.println("계속 계산하시겠습니까? (exit 입력시 종료)");
+            System.out.println("계속 계산하시겠습니까? (exit 입력시 종료 or 아무키 입력으로 continue)");
             String exit = sc.next();
             if (exit.equalsIgnoreCase("exit")) {
                 break;
